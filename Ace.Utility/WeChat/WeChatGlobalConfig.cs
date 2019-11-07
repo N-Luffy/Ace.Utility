6fe746b86a1e3f8333a94fcb5a4775baa765443e
Z-Nan <2290884623@qq.com>
2019-11-07 17:41:20 +0800

    新增微信Token管理类

---
+++ Ace.Utility/WeChat/WeChatGlobalConfig.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ace.Utility.WeChat
+{
+    /// <summary>
+    /// 微信全局配置
+    /// </summary>
+    public static class WeChatGlobalConfig
+    {
+        /// <summary>
+        /// 延迟响应微信集合
+        /// </summary>
+        // key: 微信CorpId
+        // Value: 微信Token配置
+        public static Dictionary<string, WeChatTokenConfig> StopResponseCorpIds { get; set; } = new Dictionary<string, WeChatTokenConfig>();
+
+        /// <summary>
+        /// 当前是否正在访问微信  0未获取  1获取
+        /// </summary>
+        public static int CurrentState = 0;
+    }
+
+    /// <summary>
+    /// 微信Token配置
+    /// </summary>
+    public class WeChatTokenConfig
+    {
+        /// <summary>
+        /// 最大访问微信次数
+        /// </summary>
+        public readonly int MaxTokenCount = 10;
+
+        /// <summary>
+        /// 当前请求次数
+        /// </summary>
+        public int CurrentRequestCount { get; set; } = 0;
+
+        /// <summary>
+        /// 重置请求次数时间
+        /// </summary>
+        public DateTime ResetRequestCountDate { get; set; }
+
+        /// <summary>
+        /// 延迟响应时间
+        /// </summary>
+        // 由网络错误、密钥错误或其他错误导致token未获取，将停止一段时间响应，避免不停获取次数过多导致微信限制访问
+        public DateTime StopResponseDate { get; set; }
+
+        /// <summary>
+        /// token
+        /// </summary>
+        public string Token { get; set; } = string.Empty;
+
+        /// <summary>
+        /// token过期时间
+        /// </summary>
+        public DateTime ExpiresDate { get; set; }
+    }
+}
---
+++ Ace.Utility/WeChat/WeChatTokenManagement.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using static System.String;
+
+namespace Ace.Utility.WeChat
+{
+    /// <summary>
+    /// 微信Token管理
+    /// </summary>
+    public class WeChatTokenManagement
+    {
+        /// <summary>
+        /// 获取token
+        /// </summary>
+        /// <param name="corpId">用户所属企业的corpId</param>
+        /// <param name="corpSecret">密钥</param>
+        /// <returns></returns>
+        public string GetToken(string corpId, string corpSecret)
+        {
+            string token = GetCacheToken(corpId);
+            if (!IsNullOrWhiteSpace(token)) return token;
+
+            while (IsNullOrWhiteSpace(token))
+            {
+                if (0 == Interlocked.CompareExchange(ref WeChatGlobalConfig.CurrentState, 1, 0))
+                {
+                    token = GetCacheToken(corpId);
+                    if (!IsNullOrWhiteSpace(token))
+                    {
+                        Interlocked.CompareExchange(ref WeChatGlobalConfig.CurrentState, 0, 1);
+                        break;
+                    }
+
+                    WeChatTokenConfig weChatTokenConfig = AddTokenConfig(corpId);
+
+                    if (weChatTokenConfig.CurrentRequestCount >= weChatTokenConfig.MaxTokenCount && weChatTokenConfig.ResetRequestCountDate > DateTime.Now)
+                    {
+                        Interlocked.CompareExchange(ref WeChatGlobalConfig.CurrentState, 0, 1);
+                        break;
+                    }
+
+                    if (weChatTokenConfig.ResetRequestCountDate <= DateTime.Now)
+                    {
+                        weChatTokenConfig.CurrentRequestCount = 0;
+                        weChatTokenConfig.ResetRequestCountDate = DateTime.Now.AddDays(1).Date;
+                        weChatTokenConfig.StopResponseDate = DateTime.MinValue;
+                    }
+
+                    if (weChatTokenConfig.StopResponseDate >= DateTime.Now)
+                    {
+                        Interlocked.CompareExchange(ref WeChatGlobalConfig.CurrentState, 0, 1);
+                        break;
+                    }
+
+                    try
+                    {
+                        token = GetWeChatToken(corpId, corpSecret);
+                        weChatTokenConfig.CurrentRequestCount += 1;
+                        if (token.Contains("error"))
+                        {
+                            weChatTokenConfig.StopResponseDate = DateTime.Now.AddSeconds(30);
+                        }
+                        else
+                        {
+                            weChatTokenConfig.Token = token;
+                            weChatTokenConfig.ExpiresDate = DateTime.Now.AddSeconds(7200 - 300);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        weChatTokenConfig.StopResponseDate = DateTime.Now.AddSeconds(30);
+                        Interlocked.CompareExchange(ref WeChatGlobalConfig.CurrentState, 0, 1);
+                        break;
+                    }
+                    Interlocked.CompareExchange(ref WeChatGlobalConfig.CurrentState, 0, 1);
+                }
+                else
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Thread.Sleep(1000);
+                        token = GetCacheToken(corpId);
+                        if (!IsNullOrWhiteSpace(token)) break;
+                    }
+                }
+            }
+
+            return token;
+        }
+
+        /// <summary>
+        /// 从缓存中拿到token
+        /// <param name="corpId">用户所属企业的corpId</param>
+        /// </summary>
+        /// <returns></returns>
+        public string GetCacheToken(string corpId)
+        {
+            bool v = WeChatGlobalConfig.StopResponseCorpIds.ContainsKey(corpId);
+            return v ? WeChatGlobalConfig.StopResponseCorpIds[corpId].ExpiresDate >= DateTime.Now ? WeChatGlobalConfig.StopResponseCorpIds[corpId].Token : "" : "";
+        }
+
+        /// <summary>
+        /// 新增微信token配置
+        /// </summary>
+        /// <param name="corpId">用户所属企业的corpId</param>
+        /// <returns></returns>
+        private WeChatTokenConfig AddTokenConfig(string corpId)
+        {
+            if (!WeChatGlobalConfig.StopResponseCorpIds.ContainsKey(corpId))
+            {
+                WeChatGlobalConfig.StopResponseCorpIds.Add(corpId, new WeChatTokenConfig
+                {
+                    ResetRequestCountDate = DateTime.Now.AddDays(1).Date
+                });
+            }
+
+            return WeChatGlobalConfig.StopResponseCorpIds[corpId];
+        }
+
+        /// <summary>
+        /// 获取微信token
+        /// </summary>
+        /// <param name="corpId">用户所属企业的corpId</param>
+        /// <param name="corpSecret">密钥</param>
+        /// <returns></returns>
+        public string GetWeChatToken(string corpId, string corpSecret)
+        {
+            //模拟请求 线程停止2秒
+            Thread.Sleep(2000);
+            return "test-test";
+        }
+    }
+}
