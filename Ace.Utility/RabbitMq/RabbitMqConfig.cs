9e736df0a1a5061bee44a3bdd6946c7ebae1c229
张南 <zhangnan@onlyedu.com>
2021-06-01 16:28:56 +0800

    增加RabbitMQ

---
+++ Ace.Utility/RabbitMq/RabbitMqConfig.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ace.Utility.RabbitMq
+{
+    /// <summary>
+    /// MQ配置信息
+    /// </summary>
+    public class RabbitMqConfig
+    {
+        /// <summary>
+        /// 主机名
+        /// </summary>
+        public string Host { get; set; }
+
+        /// <summary>
+        /// 心跳时间
+        /// </summary>
+        public TimeSpan? HeartBeat { get; set; }
+
+        /// <summary>
+        /// 自动重连
+        /// </summary>
+        public bool? AutomaticRecoveryEnabled { get; set; }
+
+        /// <summary>
+        /// 重连时间
+        /// </summary>
+        public TimeSpan? NetworkRecoveryInterval { get; set; }
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+    }
+
+    /// <summary>
+    /// 死信队列实体
+    /// </summary>
+    [RabbitMq("dead-letter-{Queue}", ExchangeName = "dead-letter-{exchange}")]
+    public class DeadLetterQueue
+    {
+        public string Body { get; set; }
+
+        public string Exchange { get; set; }
+
+        public string Queue { get; set; }
+
+        public string RoutingKey { get; set; }
+
+        public int RetryCount { get; set; }
+
+        public string ExceptionMsg { get; set; }
+
+        public DateTime CreateDateTime { get; set; }
+    }
+}
--- TestUnit/UnitTest.cs
+++ TestUnit/UnitTest.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using Ace.Utility;
+using Ace.Utility.RabbitMq;
 using ConsoleApp1;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace TestUnit
@@ -39,6 +43,41 @@ namespace TestUnit
                 var id = Snowflake.Instance().GetId();
                 Console.WriteLine(
                     $"开始执行 {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:ffffff")}   NO: {id} HashCode: {id.GetHashCode()}");
+            }
+        }
+
+        [Fact]
+        public void TestRabbitMq()
+        {
+            RabbitMqConfig rabbitMqConfig = new RabbitMqConfig
+            {
+                Host = "10.10.10.110",
+                UserName = "guest",
+                Password = "guest",
+            };
+            RabbitMqService rabbitMqService = new RabbitMqService(rabbitMqConfig);
+
+            Task.Factory.StartNew(() =>
+                {
+                    while (true)
+                    {
+                        rabbitMqService.Publish("test-exchange", "test-queue", "test-exchange", "{\"Data\":\"啊实打实大苏打实打实打算大苏打\"}");
+                        System.Threading.Thread.Sleep(1000);
+                    }
+                });
+
+            Task.Factory.StartNew(() =>
+            {
+                rabbitMqService.Subscribe<object>("test-queue", false, t =>
+                {
+                    var msg = JsonConvert.SerializeObject(t);
+                    Debug.WriteLine($"时间：{DateTime.Now},当前消息内容：{msg}");
+                    System.Threading.Thread.Sleep(1000);
+                }, false);
+            });
+            while (true)
+            {
+
             }
         }
     }
